fcc71ea3af659670c8d8535950d2d8ccd37320c7
Nabeil Lemar <nabeillemar@gmail.com>
2020-09-07 20:17:06 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,14 +2,14 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {},
-      :matriach => {},
+      :matriarch => {},
       :hero => {},
       :hero_friends => []
       #your key/value pairs here!
       },
     :capulet => {
       :patriarch => {},
-      :matriach => {},
+      :matriarch => {},
       :heroine => {},
       :hero_friends => []
       #your key/value pairs here!
