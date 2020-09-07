53d1a1bdc6366e3c88c6482fcc920a140684ec47
Nabeil Lemar <nabeillemar@gmail.com>
2020-09-07 20:23:01 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,30 +1,17 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
-   }, 
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
-   }
-  }
-
-  
+    :montague => {
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriach => {name: "Lady Montague", age: "54" },
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_firends => []
+      #your key/value pairs here!
+      },
+    :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriach => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :hero_firends => []
+      #your key/value pairs here!
+      }}
 end
